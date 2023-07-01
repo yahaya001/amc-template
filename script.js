4a2130009f510b8e5ea77222174027139951a8f4
yahaya001 <yahayagusau6360>
2023-07-01 11:39:53 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+function fun1(e){
+e.preventDefault();
+}
